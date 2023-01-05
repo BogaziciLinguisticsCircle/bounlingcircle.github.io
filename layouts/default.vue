37b012d9d902eb3c26065952f09feae92c4094f6
Yanek Yük <yanekyuksel@gmail.com>
2023-01-05 19:26:29 +0300

    Import Fira Sans through googleapis

--- layouts/default.vue
+++ layouts/default.vue
@@ -26,6 +26,7 @@ export default {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Fira+Sans&display=swap');
 html, body {
   background-color: #fafafa;
   font-family: "Fira Sans", sans-serif;
